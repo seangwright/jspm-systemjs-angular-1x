4814405dd63c39749063b556e6b8ba0a9cba8cc7
Sean Wright <s.galen.wright@gmail.com>
2015-10-07 19:13:00 -0400

    Updated rep module to use ui-router w/ mocked routes & placeholders in templates

--- src/js/app/representative/controller/module.js
+++ src/js/app/representative/controller/module.js
@@ -4,19 +4,46 @@ import repServiceModule from 'jsa:repService';
 
 import RepIndexController from './repIndex';
 
-let moduleName = 'jsa.representative.controllers';
+let moduleName = 'jsa.representative.controller';
 
 let controllerModule = angular.module(moduleName, [
-	
+
 	repServiceModule.name])
 
 	.controller(RepIndexController.name, RepIndexController.controller)
-	
+
 	.config(config);
-	
+
 export default controllerModule;
-	
-function config($stateProvider) {
+
+function config($stateProvider, $urlRouterProvider) {
 	"ngInject";
 	
+	// For any unmatched url, redirect to /state1
+	$urlRouterProvider.otherwise("/representatives");
+
+	// Now set up the states
+	$stateProvider
+		.state('state1', {
+			url: "/state1",
+			templateUrl: "partials/state1.html"
+		})
+		.state('state1.list', {
+			url: "/list",
+			templateUrl: "partials/state1.list.html",
+			controller: function ($scope) {
+				$scope.items = ["A", "List", "Of", "Items"];
+			}
+		})
+		.state('state2', {
+			url: "/state2",
+			templateUrl: "partials/state2.html"
+		})
+		.state('state2.list', {
+			url: "/list",
+			templateUrl: "partials/state2.list.html",
+			controller: function ($scope) {
+				$scope.things = ["A", "Set", "Of", "Things"];
+			}
+		});
 }
